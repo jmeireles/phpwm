fca4e6f8fb401d0fbfc2ad6381927886c41dfe62
dthompso99 <dthompso99@gmail.com>
2011-03-05 22:32:51 +0000

    fixed to handle the resource handles

--- classes/class.common.php
+++ classes/class.common.php
@@ -1,7 +1,9 @@
 <?php
 class button{
 	public $id;
-	function __construct($parent, $width, $height, $x, $y,  $color, $event){
+	function __construct($objCore, $parent, $width, $height, $x, $y,  $color, $event){
+		$this->_core = $objCore;
+		$this->xcb = $objCore->xcb;
 		$this->parent = $parent;
 		$this->x = $x;
 		$this->y = $y;
@@ -9,11 +11,11 @@ class button{
 		$this->height = $y;
 		$this->color = $color;
 		$this->event = $event;
-		$this->id = xcb_generate_id();
-		xcb_create_window($this->id, $this->parent , $this->width, $this->height, $this->x, $this->y, 1);
-		xcb_configure_window_events($this->id, array(4));
-		xcb_map_window($this->id);
-		xcb_flush();
+		$this->id = xcb_generate_id($this->xcb);
+		xcb_create_window($this->xcb, $this->id, $this->parent , $this->width, $this->height, $this->x, $this->y, 1);
+		xcb_configure_window_events($this->xcb, $this->id, array(4));
+		xcb_map_window($this->xcb, $this->id);
+		xcb_flush($this->xcb);
 	}
 }
 
--- phpwm.sh
+++ phpwm.sh
@@ -1,1 +1,2 @@
+Xephyr :1 &
 /usr/bin/php phpwm.php --display="127.0.0.1:1"
